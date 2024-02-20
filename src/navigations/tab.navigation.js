ee07e98e329d39d212fad6019fd3044a2a3cb2c2
nosirbek030381 <ismoilovn899@gmail.com>
2024-02-20 10:58:11 +0500

    Tab Navigation

--- src/navigations/tab.navigation.js
+++ src/navigations/tab.navigation.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import IonIcon from 'react-native-vector-icons/Ionicons';
 import { Detail, Home } from '../screens';
 
 const Tab = createBottomTabNavigator();
@@ -8,9 +9,25 @@ const Tab = createBottomTabNavigator();
 export default function TabNavigation() {
 	return (
 		<NavigationContainer>
-			<Tab.Navigator>
-				<Tab.Screen name='Home' component={Home} />
-				<Tab.Screen name='Details' component={Detail} />
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName;
+
+						if (route.name === 'Home') {
+							iconName = focused ? 'home' : 'home-outline';
+						} else if (route.name === 'Detail') {
+							iconName = focused ? 'settings' : 'settings-outline';
+						}
+
+						return <IonIcon name={iconName} size={size} color={color} />;
+					},
+					tabBarActiveTintColor: 'blue',
+					tabBarInactiveTintColor: 'black',
+				})}
+			>
+				<Tab.Screen name='Home' component={Home} options={{ headerShown: false }} />
+				<Tab.Screen name='Detail' component={Detail} options={{ headerShown: false }} />
 			</Tab.Navigator>
 		</NavigationContainer>
 	);
