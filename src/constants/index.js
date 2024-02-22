2b15a50af124c889b20d18c44d343e4eb4e26230
nosirbek030381 <ismoilovn899@gmail.com>
2024-02-22 11:44:51 +0500

    Api request

---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const api_key = 'b3394e1fb80431a3c21e8dd9f909db99';
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image, ScrollView, View } from 'react-native';
 import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { fetchTopRatedMovie, fetchTrendingMovie, fetchUpcomingMovie } from '../api';
+import TopratedMovie from '../components/toprated';
+import TrendingMovie from '../components/trending';
+import UpcomingMovie from '../components/upcoming';
 
 export default function Home({ navigation }) {
+	const [trending, setTrending] = useState([]);
+	const [upcoming, setUpcoming] = useState([]);
+	const [topRated, setTopRated] = useState([]);
+
+	useEffect(() => {
+		getTrendingMovie();
+		getUpcomingMovie();
+		getTopratedMovie();
+	}, []);
+
+	const getTrendingMovie = async () => {
+		const data = await fetchTrendingMovie();
+		setTrending(data.results);
+	};
+
+	const getUpcomingMovie = async () => {
+		const data = await fetchUpcomingMovie();
+		setUpcoming(data.results);
+	};
+
+	const getTopratedMovie = async () => {
+		const data = await fetchTopRatedMovie();
+		console.log('data', data);
+		setTopRated(data.results);
+	};
+
 	return (
 		<View className='flex-1 bg-slate-950'>
 			<SafeAreaView>
@@ -18,7 +48,11 @@ export default function Home({ navigation }) {
 			<ScrollView
 				showsVerticalScrollIndicator={false}
 				contentContainerStyle={{ paddingBottom: 20 }}
-			></ScrollView>
+			>
+				{trending.length > 0 && <TrendingMovie />}
+				{upcoming.length > 0 && <UpcomingMovie />}
+				{topRated.length > 0 && <TopratedMovie />}
+			</ScrollView>
 		</View>
 	);
 }
